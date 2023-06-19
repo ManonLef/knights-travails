d643947ee0addd0a7b4ea611c0eb798e5885d9e8
Manon <manonlefkos@gmail.com>
2023-06-19 11:45:22 +0200

    rewrite coordinates to resemble chess x-y coordinates

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,41 +1,42 @@
-function createWhiteSquare(row, square) {
+function createWhiteSquare(column, row) {
   const whiteSquare = document.createElement("div");
   whiteSquare.className = "white";
-  whiteSquare.setAttribute("data-coord", [square, row]);
+  whiteSquare.setAttribute("data-coord", [column, row]);
+  whiteSquare.textContent = ([column, row]);
   return whiteSquare;
 }
 
-function createBlackSquare(row, square) {
+function createBlackSquare(row, column) {
   const blackSquare = document.createElement("div");
   blackSquare.className = "black";
-  blackSquare.setAttribute("data-coord", [square, row]);
-
+  blackSquare.setAttribute("data-coord", [column, row]);
+  blackSquare.textContent = ([column, row]);
   return blackSquare;
 }
 
-function createBoard(row = "even", amount = 0) {
-  if (amount > 7) return;
-  let squares = 0;
+function createBoard(oddEven = "even", row = 7) {
+  if (row < 0) return;
+  let column = 0;
 
   const container = document.querySelector(".container");
 
-  if (row === "even") {
-    while (squares < 8) {
-      container.append(createWhiteSquare(amount, squares));
-      squares += 1;
-      container.append(createBlackSquare(amount, squares));
-      squares += 1;
+  if (oddEven === "even") {
+    while (column < 8) {
+      container.append(createWhiteSquare(column, row));
+      column += 1;
+      container.append(createBlackSquare(column, row));
+      column += 1;
     }
-    createBoard("odd", amount + 1);
+    createBoard("odd", row - 1);
   }
-  if (row === "odd") {
-    while (squares < 8) {
-      container.append(createBlackSquare(amount, squares));
-      squares += 1;
-      container.append(createWhiteSquare(amount, squares));
-      squares += 1;
+  if (oddEven === "odd") {
+    while (column < 8) {
+      container.append(createBlackSquare(column, row));
+      column += 1;
+      container.append(createWhiteSquare(column, row));
+      column += 1;
     }
-    createBoard("even", amount + 1);
+    createBoard("even", row - 1);
   }
 }
 
@@ -44,27 +45,27 @@ function addKnight() {
   squares[35].textContent = "♘";
 
   const attribute = squares[35].getAttribute("data-coord");
-  console.log("Knight's coordinates: ", [
-    parseFloat(attribute[0]),
-    parseFloat(attribute[2]),
-  ]);
+  const knightPosition = [parseFloat(attribute[0]), parseFloat(attribute[2])];
+  console.log("Knight's coordinates: ", knightPosition);
   // random knight position
   // const randomSquare = Math.floor(Math.random() * 64);
   // squares[randomSquare].textContent = "♘"
+  return knightPosition;
 }
 
 function createBoardArray() {
   const squares = document.querySelector(".container").childNodes;
   const gameArray = [];
   squares.forEach((node) => {
-    const data = node.getAttribute("data-coord")
-    const coord = [parseFloat(data[0]), parseFloat(data[2])]
-    gameArray.push(coord)
-  })
-  console.log(gameArray[7])
-  return gameArray
+    const data = node.getAttribute("data-coord");
+    const coord = [parseFloat(data[0]), parseFloat(data[2])];
+    gameArray.push(coord);
+  });
+  return gameArray;
 }
 
 createBoard();
 addKnight();
-createBoardArray()
+createBoardArray();
+
+export { addKnight, createBoardArray };
