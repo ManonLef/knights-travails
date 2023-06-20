8e09e79fd0dbb7d6a5b8eb280fa39855b7a856ba
Manon <manonlefkos@gmail.com>
2023-06-20 22:41:40 +0200

    add functions file with helpers and testing area

---
+++ src/functions.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-restricted-syntax */
+import GameBoard from "./DOMgameBoard";
+import { logStringGameArray, clog } from "./debug";
+import Knight from "./DOMknight";
+
+/// ///////////////////////
+//  initial game render  //
+/// ///////////////////////
+
+const myBoard = new GameBoard();
+const chessBoard = [...myBoard.boardArray];
+const knight = new Knight("default");
+
+/// ////////////////////////
+// array helper functions //
+/// ////////////////////////
+
+// returns -1 if not found, otherwise returns index
+function arrayIncludes(arr, compare) {
+  let index = -1;
+  for (const array of compare) {
+    index += 1;
+    if (array[0] === arr[0] && array[1] === arr[1]) {
+      return index;
+    }
+  }
+  return -1;
+}
+
+/// ////////////////////////
+//       testing area     //
+/// ////////////////////////
+
+logStringGameArray(chessBoard);
+clog(`knight's coordinates are: [${knight.coord}]`);
