f00e94bd0f59a86d50231bed05eb68cdc7953f3a
Manon <manonlefkos@gmail.com>
2023-06-21 00:59:10 +0200

    add move testing functions

--- src/moves.js
+++ src/moves.js
@@ -9,6 +9,11 @@ import { logArray, clog } from "./debug";
 const start = getKnightPosition();
 const end = [4, 5];
 
+const indexStart = arrayIncludes(start, getBoard());
+removeFromBoard(indexStart)
+console.log("the board with the starting position removed. Has length", getBoard().length)
+logArray(getBoard())
+
 const moves = [
   [-1, -2],
   [-2, -1],
@@ -20,13 +25,21 @@ const moves = [
   [+2, -1],
 ];
 
-function getNextMoves(start, moves) {
+function getNextMoves(coord) {
   const possibleMoves = [];
   for (let i = 0; i < moves.length; i++) {
-    const col = start[0] + moves[i][0];
-    const row = start[1] + moves[i][1];
+    const col = coord[0] + moves[i][0];
+    const row = coord[1] + moves[i][1];
     if (col >= 0 && row >= 0 && col <= 7 && row <= 7) {
-      possibleMoves.push([col, row]);
+      const move = [col, row];
+      const visited = arrayIncludes(move, getBoard());
+      if (visited >= 0) {
+        // visited will be -1 (so true) if the array is already visited and removed
+        // here we check for unvisited and push it and also removing from game array
+        removeFromBoard(visited);
+        possibleMoves.push(move);
+        // consider moving the removal into another function
+      }
     }
   }
   return possibleMoves;
@@ -38,20 +51,71 @@ function getNextMoves(start, moves) {
 
 clog(`we start at [${start}] and want to find the shortest route to [${end}]`);
 
-logArray(getNextMoves(start, moves));
-
-function getNextLevel(array, moves) {
+function getNextLevel(array) {
   const nextLevel = [];
+  const nextLevelSeparate = [];
   for (let i = 0; i < array.length; i++) {
     // only push when in game array
     const nextArray = getNextMoves(array[i], moves);
-    // make sure
     nextLevel.push(nextArray);
-    // for (let j = 0; j < nextArray.length; j++) {
-    //   nextLevel.push(nextArray[j])
+    for (let j = 0; j < nextArray.length; j++) {
+      nextLevelSeparate.push(nextArray[j]);
+    }
   }
-  logArray(nextLevel);
-  console.log(nextLevel.length); // handy because these correspond with the first array
+  return [nextLevel, nextLevelSeparate];
 }
 
-getNextLevel(getNextMoves(start, moves), moves);
+// when getting next moves:
+// - get moves
+//    - check if they're visited yet (still in game array)
+//    - if not, push
+//    - remove from game array
+//    - if already visited, skip
+const levelOne = getNextMoves(start);
+
+console.log("level 1 with length ", levelOne.length, "leaves gameboard at ", getBoard().length);
+logArray(levelOne);
+
+const levelTwo = getNextLevel(levelOne);
+const levelTwoNested = levelTwo[0];
+const levelTwoSanitized = levelTwo[1];
+
+console.log("level two");
+logArray(levelTwoNested);
+console.log("level two nested length is ", levelTwoNested.length);
+levelTwoNested.forEach((element) => {
+  console.log("level two elements");
+  logArray(element);
+});
+console.log(
+  "a sanitized level two version can be found here, handy for level three ;), with length",
+  levelTwoSanitized.length, "leaves gameboard at ", getBoard().length
+);
+logArray(levelTwoSanitized);
+
+const levelThree = getNextLevel(levelTwoSanitized);
+
+console.log(
+  "levelThree is where it gets tricky, it has length of ",
+  levelThree[1].length,
+  "if you don't count the empty children", "leaves gameboard at ", getBoard().length
+);
+logArray(levelThree);
+console.log("unsanitized");
+logArray(levelThree[0]);
+console.log(
+  "sanitized also removes empty elements which might make it harder to tie to parent"
+);
+logArray(levelThree[1]);
+
+console.log("the board now looks like this:", getBoard().length);
+logArray(getBoard());
+
+// next steps would be tying these up as children to the starting nodes
+// BFS needs a queue
+// I'd first queue the initial item
+// then calculate next steps from that
+// check if the array that's output has the end position
+// if not, pass each item in the queue as next step, with the passed item getting a reference to the child,
+// and the child getting a reference back to the parent
+// rinse and repeat
