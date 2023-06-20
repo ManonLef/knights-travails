31ff3426d12bda4200e78e834170d8a754e92fc9
Manon <manonlefkos@gmail.com>
2023-06-20 20:43:47 +0200

    add console log function for more readable nested arrays

---
+++ src/debug.js
@@ -0,0 +1,8 @@
+// print helpers
+
+// console.log array data in more readable string format with JSON
+function logStringGameArray(x) {
+  console.log(JSON.stringify(x));
+}
+
+export { logStringGameArray };
