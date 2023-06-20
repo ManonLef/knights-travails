93b0b05a3b5c0bd8938f44887aa3e22b6ff7eb5b
Manon <manonlefkos@gmail.com>
2023-06-20 23:27:38 +0200

    create function to output array of next moves

--- src/moves.js
+++ src/moves.js
@@ -4,7 +4,7 @@ import {
   getBoard,
   getKnightPosition,
 } from "./functions";
-import { logStringGameArray, clog } from "./debug";
+import { logArray, clog } from "./debug";
 
 const start = getKnightPosition();
 const end = [4, 5];
@@ -20,8 +20,22 @@ const moves = [
   [+2, -1],
 ];
 
+function getNextMoves(start, moves) {
+  const possibleMoves = [];
+  for (let i = 0; i < moves.length; i++) {
+    const col = start[0] + moves[i][0];
+    const row = start[1] + moves[i][1];
+    if (col >= 0 && row >= 0 && col <= 7 && row <= 7) {
+      possibleMoves.push([col, row]);
+    }
+  }
+  return possibleMoves
+}
+
 /// ////////////////////////
 //       testing area     //
 /// ////////////////////////
 
 clog(`we start at [${start}] and want to find the shortest route to [${end}]`);
+
+logArray(getNextMoves(start, moves));
